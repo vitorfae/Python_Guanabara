b701b1f037f8161932e904a2a0f20900287ceb89
Vitor Matheus Fae Ulrich <vitor.ulrich@copercampos.com.br>
2025-05-08 18:14:14 -0300

    Aula 07 - parte 1

---
+++ PythonExercicios/ex003.py
@@ -0,0 +1,4 @@
+numero1 = int(input('Digite um valor: '))
+numero2 = int(input('Digite outro valor: '))
+soma = numero1 + numero2
+print('A soma entre {} e {} vale {}'.format(numero1, numero2, soma))
