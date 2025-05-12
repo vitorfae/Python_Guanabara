8f57b98d0b69c5acd1cd8698c0c2cde3a4148544
Vitor Matheus Fae Ulrich <vitor.ulrich@copercampos.com.br>
2025-05-12 17:51:32 -0300

    Aula 07 - exercícios e ajustes

--- PythonExercicios/ex009.py
+++ PythonExercicios/ex009.py
@@ -1,6 +1,8 @@
 tabuada = int(input('Digite um número para saber a tabuada do mesmo:'))
 i = 0
+print('-' * 12)
 while (i<=10):
     resultado = tabuada * i
-    print('{} X {} = {}'.format(tabuada, i, resultado))
+    print('{} X {:2} = {}'.format(tabuada, i, resultado))
     i += 1
+print('-' * 12)
--- PythonExercicios/ex010.py
+++ PythonExercicios/ex010.py
@@ -1,4 +1,4 @@
 reais = float(input('Qual valor tem na sua carteira? R$'))
 dolar = float(3.27)
 dinheiro = reais / dolar
-print('Você tem R${} reais na carteira, o dólar está no valor de ${}, o dinheiro que você tem na carteira equivale a ${} dólares'.format(reais, dolar, dinheiro))
+print('Você tem R${} reais na carteira, o dólar está no valor de U${}, o dinheiro que você tem na carteira equivale a U${:.2f} dólares'.format(reais, dolar, dinheiro))
