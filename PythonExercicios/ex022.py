765c76fd4f9bec6b42ee4d8938b018eaebaea0d7
Vitor Matheus Fae Ulrich <vitor.ulrich@copercampos.com.br>
2025-05-16 17:28:47 -0300

    observando métodos utilizados pelo guanabara

--- PythonExercicios/ex022.py
+++ PythonExercicios/ex022.py
@@ -4,4 +4,14 @@ print('Seu nome em maiúsculas é {}'.format(nome.upper()))
 print('Seu nome em minúsculas é {}'.format(nome.lower()))
 print('Seu nome contando os espaços tem {} caracteres'.format(len(nome)))
 print('Seu nome contando apenas as letras tem {} caracteres'.format(len(''.join(nome.split())))) #nome split vai separar cada palavra em uma array, join com o '' junta todos novamente sem espaços e o len conta a quantidade sem espaços
-print('O seu primeiro nome é {} e tem {} letras'.format(len(nome.split, nome.split()[0]))) #o objeto nome está sendo separado palavras em indices da arrar, o [0] fora do parenteses está invocando o primeiro indice e o len vendo quantos caracteres tem.
+print('O seu primeiro nome é {} e tem {} letras'.format(nome.split()[0], len(nome.split()[0]))) #o objeto nome está sendo separado palavras em indices da arrar, o [0] fora do parenteses está invocando o primeiro indice e o len vendo quantos caracteres tem.
+
+
+#jeito do guanabara de contar as letras sem espaço -> (len(nome)) - nome.count(' ')
+# o primeiro aqui de cima (len(nome)) vai contar os caracteres - (menos) nome.count(' ') -> diminui o valor dos caracteres espaços
+
+#Jeito guanabara de achar o primeiro nome -> nome.find(' '), pois vai entregar onde está o primeiro espaço, que significa o primeiro nome
+
+#pode ser feio o último da maneira a seguir:
+# criar um array -> separa = nome.split() -> vai separar por palavras
+#dai para exibir o primeiro nome chama da seguinte maneira -> separa[0] e para ver o tamanho -> len(separa[0])
--- PythonExercicios/ex023.py
+++ PythonExercicios/ex023.py
@@ -1,6 +1,6 @@
 numero = input('Digite um número de 0 a 9999: ')
 
-print(len(numero))
+print('Analisando o número {}'.format(numero))
 if len(numero) == 4:
     print('milhar {}'.format(numero[0]))
     print('centena {}'.format(numero[1]))
@@ -15,3 +15,14 @@ elif len(numero) == 2:
     print('unidade {}'.format(numero[1]))
 elif len(numero) == 1:
     print('unidade {}'.format(numero[0]))
+
+# modo guanabara
+#numero = int(input('informe um número'))
+#unidade = numero // 1 % 10 -> ou seja ele está usando a divisão inteira por conta do // e o resto da divisão por 10.
+#dezena = numero // 10 % 10
+#centena = numero // 100 % 10
+#milhar = numero // 1000 % 10
+#print('Unidade: {}'.format(unidade))
+#print('Dezena: {}'.format(dezena))
+#print('centena: {}'.format(centena))
+#print('Milhar: {}'.format(milhar))
