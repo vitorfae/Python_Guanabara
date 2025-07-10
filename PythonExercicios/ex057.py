c70e4c2f3265ed53aab1952ac79569d86f4c3097
unknown <vitorfaeulrich0@gmail.com>
2025-07-10 12:28:15 -0300

    correção

--- PythonExercicios/ex057.py
+++ PythonExercicios/ex057.py
@@ -1,9 +1,18 @@
+#MEU JEITO
 sexo = ''
 while sexo != 'M' and sexo != 'F':
-    sexo = str(input('Qual o seu sexo? [M/F] ')).upper()
+    sexo = str(input('Qual o seu sexo? [M/F] ')).strip().upper()[0] # 0 vai pegar a primeira letra e o strip remover espacos
     if sexo == 'M':
         print('Sexo Masculino')
     elif sexo == 'F':
         print('Sexo Feminino')
     else:
         print('Opcao invalida, tente novamente')
+
+#JEITO GUANABARA
+'''
+sexo = str(input('informe seu sexo? [M/F] ')).strip().upper()[0]
+while sexo not in 'MF':
+    sexo = str(input('Dados invalidos. Por favor, informe seu sexo: [M/F] ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso'.format(sexo))
+'''
--- PythonExercicios/ex058.py
+++ PythonExercicios/ex058.py
@@ -1,10 +1,11 @@
+#MEU JEITO
 import random
 from time import sleep
 
 tentativas = 0
 resposta = 0
 print('-=-'*21)
-print('O computador vai sortear um numero de 1 a 10, tente adivinhar!')
+print('O computador vai sortear um numero de 0 a 10, tente adivinhar!')
 print('-=-'*21)
 print('ESTOU PENSANDO QUAL NUMERO IREI ESCOLHER...')
 for c in range(1, 22):
@@ -12,8 +13,23 @@ for c in range(1, 22):
     sleep(0.3)
 print('')
 
-numeroAleatorio = random.randint(1, 10)
+numeroAleatorio = random.randint(0, 10)
 while resposta != numeroAleatorio:
     resposta = int(input('Qual o numero que o computador sorteou?'))
     tentativas += 1
 print('Parabens voce ACERTOU o numero sorteado ({}) com {} tentaivas'.format(numeroAleatorio, tentativas))
+
+
+#JEITO GUANABARA
+'''
+from random import randint
+computador = randint(0, 10)
+print('Sou seu computador... Acabei de pensar em um numero entre 0 e 10')
+print('Sera que voce consegue adivinhar qual foi?')
+acertou = False
+while not acertou:
+    jogador = int(input('Qual e seu palpite? '))
+    if jogador == computador:
+    acertou = True
+    
+'''
