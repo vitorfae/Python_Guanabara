eac0a4d2f2ce312a9515e7b16f2356eba88e0f5c
unknown <vitorfaeulrich0@gmail.com>
2025-07-23 21:14:56 -0300

    Exercício 075 resolvido

--- PythonExercicios/ex075.py
+++ PythonExercicios/ex075.py
@@ -8,3 +8,14 @@ tupla = (valor1, valor2, valor3, valor4)
 print(f'Voce digitou os valores {tupla}')
 print(f'O valor 9 apareceu {tupla.count(9)} vezes')
 
+for pos, numero in enumerate(tupla):
+    if numero == 3:
+        print(f'O valor 3 apareceu na {pos}ª posicao')
+if 3 not in tupla:
+    print('O valor 3 nao foi digitado em nenhuma posicao')
+
+par = () #Fica fora, senao em todas as vezes que o for for executado ele vai criar a tupla
+for c in range(0, len(tupla)):
+    if tupla[c] % 2 == 0:
+        par = par + (tupla[c],)
+print(f'Os valores pares foram: {par}')
