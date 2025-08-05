c6a3d76eb8c8b5b333aca9ea3594b2e63904be7e
Vitor Faé <vitorfaeulrich0@gmail.com>
2025-08-05 20:21:47 -0300

    Exercício 78 corrigido

--- PythonExercicios/ex078.py
+++ PythonExercicios/ex078.py
@@ -20,6 +20,33 @@ for indice, valor in enumerate(valores):
 print(f'O maior valor digitado foi {maior} nas posições {indiceMaior}')
 print(f'O menor valor digitado foi {menor} nas posições {indiceMenor}')
 
+#JEITO GUANABARRA
+'''
+listanum = []
+maior = 0
+menor = 0
+for c in range(0, 5):
+    listanum.append(int(input(f'Digite um valor para a posição {c}: ')))
+    if c == 0:
+        maior = menor = listanum[c]
+    else:
+        if listanum[c] > mai:
+            maior = listanum[c]
+        if listanum[c] < men:
+            menor = listanum[c]
+print('=-'*20)
+print(f'Você digitou os valores {listanum}')
+print(f'O maior valor digitado foi {maior} nas posições', end='')
+for indice, valor in enumerate(listanum):
+    if valor == maior:
+        print(f'{indice}...', end='')}')
+print()
+print(f'O menor valor digitado foi {menor} nas posições', end='')
+for indice, valor in enumerate(listanum):
+    if valor == menor:
+        print(f'{indice}...', end='')
+            
+'''
 
 
 
