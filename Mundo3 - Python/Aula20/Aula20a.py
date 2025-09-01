179fb46efc3c1483e83b85bc993aa05266ad87f4
unknown <vitorfaeulrich0@gmail.com>
2025-09-01 12:50:52 -0300

    Aula 20

---
+++ Mundo3 - Python/Aula20/Aula20a.py
@@ -0,0 +1,14 @@
+def linhaPontilhada():
+    print('-' * 30)
+
+linhaPontilhada()
+print(' CURSO EM VIDEO ')
+linhaPontilhada()
+
+linhaPontilhada()
+print(' APRENDA PYTHON ')
+linhaPontilhada()
+
+linhaPontilhada()
+print(' VITOR FAE ')
+linhaPontilhada()
