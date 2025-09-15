a0187f21dffcbed0b2fb9a2f7a92b8688888afe2
Vitor Matheus Fae Ulrich <vitor.ulrich@copercampos.com.br>
2025-09-15 18:14:18 -0300

    Cabeçalhos prontos ex115

--- PythonExercicios/ex115/lib/interface/__init__.py
+++ PythonExercicios/ex115/lib/interface/__init__.py
@@ -1,3 +1,16 @@
+def leiaInt(numero):
+    while True:
+        try:
+            inteiro = int(input(numero))
+        except KeyboardInterrupt:
+            print('\n\033[031mO usuário prefiriu não digitar esse número\033[m')
+            return 0
+
+        except:
+            print('\033[031mERRO: por favor, digite um número inteiro válido.\033[m')
+        else:
+            return inteiro
+
 def linha(tamanho = 42):
     return '-' * tamanho
 
@@ -5,3 +18,13 @@ def cabecalho(texto):
     print(linha())
     print(texto.center(42))
     print(linha())
+
+def menu(lista):
+    cabecalho('MENU PRINCIPAL')
+    contador = 1
+    for item in lista:
+        print(f'\033[33m{contador}\033[m - \033[34m{item}\033[m')
+        contador += 1
+    print(linha())
+    opcao = leiaInt('\033[32mSua Opção: \033[m')
+    return opcao
--- PythonExercicios/ex115teste.py
+++ PythonExercicios/ex115teste.py
@@ -1,6 +1,18 @@
 from ex115.lib.interface import *
+from time import sleep
 
-cabecalho('MENU PRINCIPAL')
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        cabecalho('opção 1')
+    elif resposta == 2:
+        cabecalho('opção 2')
+    elif resposta == 3:
+        cabecalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        print('\033[31mErro! Digite uma opção inválida\033[m')
+    sleep(2)
 
 
 
