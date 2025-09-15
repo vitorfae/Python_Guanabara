cf5364275e8a9778f25d71dae1135389b9a6fe96
Vitor Matheus Fae Ulrich <vitor.ulrich@copercampos.com.br>
2025-09-15 17:42:34 -0300

    Exercício 115

---
+++ PythonExercicios/ex115teste.py
@@ -0,0 +1,23 @@
+from ex115.lib.interface import *
+
+cabecalho('MENU PRINCIPAL')
+
+
+
+
+
+
+
+
+
+'''print('-'*30)
+print('MENU PRINCIPAL'.center(30))
+print('-'*30)
+print('\033[033m1 -\033[m', end=' ')
+print('\033[034mVer pessoas cadastradas')
+print('\033[033m2 -\033[m', end=' ')
+print('\033[034mCadastrar nova pessoa')
+print('\033[033m3 -\033[m', end=' ')
+print('\033[034mSair do sistema')
+
+opcao = int(input('Sua Opção: '))'''
